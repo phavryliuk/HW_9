3396f62a44f7544382d5f98bdf9ee95eddcfff03
phavryliuk <petrrrusyk93@gmail.com>
2022-11-03 21:30:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿public class Library
+{
+    public static string BookName { get; set; }
+    public static string BookAuthor { get; set; }
+    public static int BookId { get; set; }
+}
+
+
+public class Output
+{ 
+    public static void Main()
+
+    {
+            Library.BookName = "Kobzar";
+            Library.BookAuthor = "Taras Shevchenko";
+            Library.BookId = 0001;
+           
+            Console.WriteLine(Library.BookName);
+            Console.WriteLine(Library.BookAuthor);
+            Console.WriteLine(Library.BookId);
+
+        
+
+    }
+}
+
